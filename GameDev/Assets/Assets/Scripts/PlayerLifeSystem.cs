374ac32c75dcc04abe7fc3c7b6ff46bd6764d9f7
LW Wong <lawwlw@connect.hku.hk>
2025-04-25 02:38:49 +0800

    update

--- GameDev/Assets/Assets/Scripts/PlayerLifeSystem.cs
+++ GameDev/Assets/Assets/Scripts/PlayerLifeSystem.cs
@@ -3,7 +3,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLifeSystem : MonoBehaviour // Renamed to reflect its purpose
+// public class PlayerLifeSystem : MonoBehaviour // Renamed to reflect its purpose
+// {
+//     public static int life = 5;
+//     public GameObject redHearts;
+//     public GameObject whiteHearts;
+//     private GameObject[] instantiatedHearts;
+//     public Vector2 firstHeart = new Vector2(-8f, 4f);
+
+//     void Start()
+//     {
+//         instantiatedHearts = new GameObject[5];
+//         InitializeHearts();
+//     }
+
+//     void InitializeHearts()
+//     {
+//         for (int i = 0; i < 5; i++)
+//         {
+//             Vector2 heartPosition = new Vector2(firstHeart.x + i, firstHeart.y);
+//             instantiatedHearts[i] = Instantiate(redHearts, heartPosition, Quaternion.identity);
+//         }
+//     }
+
+//     void OnTriggerEnter2D(Collider2D obj)
+//     {
+//         DestroyAllHearts();
+//         string name = obj.gameObject.name;
+
+//         if (name == "enemy")
+//         {
+//             life -= 1;
+//         }
+//         else if (name == "food")
+//         {
+//             if (life != 5)
+//             {
+//                 life += 1;
+//             }
+//         }
+        
+//         UpdateHearts();
+//     }
+
+//     void UpdateHearts()
+//     {
+//         for (int i = 0; i < 5; i++)
+//         {
+//             Vector2 heartPosition = new Vector2(firstHeart.x + i, firstHeart.y);
+//             instantiatedHearts[i] = Instantiate(
+//                 i < life ? redHearts : whiteHearts, 
+//                 heartPosition, 
+//                 Quaternion.identity
+//             );
+//         }
+//     }
+
+//     void OnGUI()
+//     {
+//         if (life == 0)
+//         {
+//             GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2, 500, 100), "Game Over!");
+//             Time.timeScale = 0;
+//         }
+//     }
+
+//     void DestroyAllHearts()
+//     {
+//         foreach (GameObject obj in instantiatedHearts)
+//         {
+//             if (obj != null)
+//             {
+//                 Destroy(obj);
+//             }
+//         }
+//     }
+// }
+
+// using UnityEngine;
+
+public class PlayerLifeSystem : MonoBehaviour
 {
     public static int life = 5;
     public GameObject redHearts;
@@ -26,23 +105,20 @@ public class PlayerLifeSystem : MonoBehaviour // Renamed to reflect its purpose
         }
     }
 
-    void OnTriggerEnter2D(Collider2D obj)
+    void OnTriggerEnter2D(Collider2D other)
     {
         DestroyAllHearts();
-        string name = obj.gameObject.name;
 
-        if (name == "enemy")
+        // Check by tag instead of name
+        if (other.CompareTag("Enemy"))
         {
-            life -= 1;
+            life = Mathf.Max(0, life - 1); // Ensures life doesn't go below 0
         }
-        else if (name == "food")
+        else if (other.CompareTag("Food"))
         {
-            if (life != 5)
-            {
-                life += 1;
-            }
+            life = Mathf.Min(5, life + 1); // Ensures life doesn't exceed 5
         }
-        
+
         UpdateHearts();
     }
 
